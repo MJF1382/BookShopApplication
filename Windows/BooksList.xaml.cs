3f3e758f3d7c29ca78641fe6e0224eae5235406c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-08 10:25:16 -0700

    Create "BooksList" window.

--- Windows/BooksList.xaml.cs
+++ Windows/BooksList.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BookShopApplication
+namespace BookShopApplication.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BooksList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BooksList : Window
     {
-        public MainWindow()
+        public BooksList()
         {
             InitializeComponent();
         }
