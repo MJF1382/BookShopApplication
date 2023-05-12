3efa4fca19da95d6b5084fa8ff986629604d41e1
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-12 01:27:50 -0700

    Adding "IconButton" user control.

---
+++ Views/UserControls/IconButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace BookShopApplication.Views.UserControls
+{
+    public partial class IconButton : UserControl
+    {
+        private string source;
+        public string Source
+        {
+            get { return source; }
+            set
+            {
+                source = value;
+                imgIcon.Source = new BitmapImage(new System.Uri(source, System.UriKind.Relative));
+            }
+        }
+
+        public IconButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
