9f78f569c8933ca1a7f5712eb3b8f4524bb5e8ca
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-12 07:35:03 -0700

    Changing "Button" to "PanelMenuButton".

---
+++ Views/UserControls/PanelMenuButton.xaml.cs
@@ -0,0 +1,103 @@
+﻿using BookShopApplication.Extensions;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BookShopApplication.Views.UserControls
+{
+    public partial class PanelMenuButton : UserControl
+    {
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                txbText.Text = text;
+            }
+        }
+
+        private string iconSource;
+        public string IconSource
+        {
+            get { return iconSource; }
+            set
+            {
+                iconSource = value;
+                imgIcon.Source = new BitmapImage(new System.Uri(iconSource, System.UriKind.Relative));
+            }
+        }
+
+        private string contentColor;
+        public string ContentColor
+        {
+            get { return contentColor; }
+            set
+            {
+                contentColor = value;
+                txbText.Foreground = new BrushConverter().ConvertFrom(contentColor) as Brush; ;
+            }
+        }
+
+        private double contentSize;
+        public double ContentSize
+        {
+            get { return contentSize; }
+            set
+            {
+                contentSize = value;
+                txbText.FontSize = contentSize;
+            }
+        }
+
+        private string buttonBackground;
+        public string ButtonBackground
+        {
+            get { return buttonBackground; }
+            set
+            {
+                if (value.HasValue())
+                {
+                    buttonBackground = value;
+                }
+                else
+                {
+                    buttonBackground = "#00FFFFFF";
+                }
+
+                buttonBackground = value;
+                btn.Background = new BrushConverter().ConvertFrom(buttonBackground) as Brush; ;
+            }
+        }
+
+        private double buttonWidth;
+        public double ButtonWidth
+        {
+            get { return buttonWidth; }
+            set
+            {
+                buttonWidth = value;
+                btn.Width = buttonWidth;
+                stackPanel.Width = buttonWidth;
+            }
+        }
+
+        private double buttonHeight;
+        public double ButtonHeight
+        {
+            get { return buttonHeight; }
+            set
+            {
+                buttonHeight = value;
+                btn.Height = buttonHeight;
+                stackPanel.Height = buttonHeight;
+            }
+        }
+
+        public PanelMenuButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
