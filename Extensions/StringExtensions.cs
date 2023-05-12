facd4321b499bc421fdf27385aea34e8a291b0c2
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-12 02:56:19 -0700

    Adding "StringExtensions" file.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool HasValue(this string input)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
