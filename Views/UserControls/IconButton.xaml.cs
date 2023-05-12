e3e018bfb61cce2c92129ffe38605e558378c4fd
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-12 02:55:51 -0700

    Updating "IconButton".

--- Views/UserControls/IconButton.xaml.cs
+++ Views/UserControls/IconButton.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using BookShopApplication.Extensions;
 
 namespace BookShopApplication.Views.UserControls
 {
@@ -13,6 +15,69 @@ namespace BookShopApplication.Views.UserControls
             {
                 source = value;
                 imgIcon.Source = new BitmapImage(new System.Uri(source, System.UriKind.Relative));
+            }
+        }
+
+        private double iconWidth;
+        public double IconWidth
+        {
+            get { return iconWidth; }
+            set
+            {
+                iconWidth = value;
+                imgIcon.Width = iconWidth;
+            }
+        }
+
+        private double iconHeight;
+        public double IconHeight
+        {
+            get { return iconHeight; }
+            set
+            {
+                iconHeight = value;
+                imgIcon.Height = iconHeight;
+            }
+        }
+
+        private string buttonBackground;
+        public string ButtonBackground
+        {
+            get { return buttonBackground; }
+            set
+            {
+                if (value.HasValue())
+                {
+                    buttonBackground = value;
+                }
+                else
+                {
+                    buttonBackground = "#00FFFFFF";
+                }
+
+                btnIcon.Background = new BrushConverter().ConvertFrom(buttonBackground) as Brush;
+            }
+        }
+
+        private double buttonWidth;
+        public double ButtonWidth
+        {
+            get { return buttonWidth; }
+            set
+            {
+                buttonWidth = value;
+                btnIcon.Width = buttonWidth;
+            }
+        }
+
+        private double buttonHeight;
+        public double ButtonHeight
+        {
+            get { return buttonHeight; }
+            set
+            {
+                buttonHeight = value;
+                btnIcon.Height = buttonHeight;
             }
         }
 
