2ee86309e21c8a1ea38a42239e927aafa52e2e98
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-13 10:10:46 -0700

    Changing "StringExtensions" directory.

--- Helpers/Extensions/StringExtensions.cs
+++ Helpers/Extensions/StringExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BookShopApplication.Extensions
+namespace BookShopApplication.Helpers.Extensions
 {
     public static class StringExtensions
     {
