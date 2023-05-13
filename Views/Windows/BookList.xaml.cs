f27c449a08cbffa955950745258cdadf4c219062
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-13 10:17:39 -0700

    Adding "BookList" window.

---
+++ Views/Windows/BookList.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace BookShopApplication.Views.Windows
+{
+    public partial class BookList : Window
+    {
+        DataBase.BookShopDBContext _context = new DataBase.BookShopDBContext();
+
+        public BookList()
+        {
+        }
+    }
+}
