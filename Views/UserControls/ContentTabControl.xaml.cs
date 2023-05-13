37722c99dae997798dcc37df94ae4b5eba83d115
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-13 10:16:17 -0700

    Adding "ContentTabControl" user control.

---
+++ Views/UserControls/ContentTabControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using BookShopApplication.DataBase;
+using BookShopApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookShopApplication.Views.UserControls
+{
+    public partial class ContentTabControl : UserControl
+    {
+        public ContentTabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
