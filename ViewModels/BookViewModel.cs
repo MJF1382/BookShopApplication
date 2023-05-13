77867280943566181d139841428aa6504a844ec6
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-13 10:13:43 -0700

    Adding "BookViewModel".

---
+++ ViewModels/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Isbn { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string CategoryName { get; set; }
+        public int Price { get; set; }
+    }
+}
