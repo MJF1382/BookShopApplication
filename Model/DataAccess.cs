f3ef4616ae665fac71d554d321bfb6be2cc86160
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-15 11:02:13 -0700

    Adding "DataAccess" file to get data from database.

---
+++ Model/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using BookShopApplication.DataBase;
+using BookShopApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Model
+{
+    public static class DataAccess
+    {
+        private static ObservableCollection<BookViewModel> books;
+        public static ObservableCollection<BookViewModel> Books
+        {
+            get { return books; }
+            set
+            {
+                books = value;
+            }
+        }
+
+        static DataAccess()
+        {
+            books = new ObservableCollection<BookViewModel>();
+        }
+    }
+}
