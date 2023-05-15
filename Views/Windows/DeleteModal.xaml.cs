695eabbfbdb46ab6412a12f981d231587a073370
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-15 11:14:24 -0700

    Modifying "DeleteModal" window.

--- Views/Windows/DeleteModal.xaml.cs
+++ Views/Windows/DeleteModal.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using BookShopApplication.DataBase;
 using BookShopApplication.Helpers.Enums;
+using BookShopApplication.ViewModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,26 +21,69 @@ namespace BookShopApplication.Views.Windows
     public partial class DeleteModal : Window
     {
         private object _identifier;
+        private EntityType _entityType;
         private BookShopDBContext _context = new BookShopDBContext();
 
         public DeleteModal(Window owner, object identifier, EntityType entityType)
         {
             Owner = owner;
             _identifier = identifier;
+            _entityType = entityType;
 
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Close_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            switch (_entityType)
+            {
+                case EntityType.Book:
+                    Book book = _context.Books.Find(_identifier);
+                    _context.Books.Remove(book);
+                    bool result = Convert.ToBoolean(_context.SaveChanges());
 
+                    if (result)
+                    {
+                        BookViewModel modelBook = Model.DataAccess.Books.Single(p => p.Isbn == book.Isbn);
 
-            //_context.Books.Remove(new Book() { Isbn = _identifier })
+                        bool res = Model.DataAccess.Books.Remove(modelBook);
+
+                        Close();
+
+                        SuccessMessageModal successMessageModal = new SuccessMessageModal(App.Current.MainWindow, "کتاب با موفقیت ثبت شد.");
+                        successMessageModal.ShowDialog();
+                    }
+                    else
+                    {
+                        this.Background = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+                        main.Opacity = 0.25;
+
+                        ErrorMessageModal errorMessageModal = new ErrorMessageModal(this, "خطا در اتصال به سرور، لطفا بعدا دوباره امتحان کنید.");
+                        errorMessageModal.ShowDialog();
+
+                        this.Background = new SolidColorBrush(Color.FromArgb(0, 255, 255, 255));
+                        main.Opacity = 1;
+                    }
+
+                    break;
+                case EntityType.Category:
+                    break;
+                case EntityType.Customer:
+                    break;
+                case EntityType.Order:
+                    break;
+                case EntityType.OrderBook:
+                    break;
+                default:
+                    break;
+            }
+
+            Close();
         }
     }
 }
