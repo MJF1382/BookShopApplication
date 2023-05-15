4844163c5471029a3c1e7a70aacdac99915644d8
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-15 11:06:52 -0700

    Modifying "BodyContent" user control.

--- Views/UserControls/BodyContent.xaml.cs
+++ Views/UserControls/BodyContent.xaml.cs
@@ -21,41 +21,36 @@ namespace BookShopApplication.Views.UserControls
     public partial class BodyContent : UserControl
     {
         BookShopDBContext _context = new BookShopDBContext();
-        public List<BookViewModel> Books { get; set; }
 
         public BodyContent()
         {
             InitializeComponent();
-
-            Books = (from book in _context.Books
-                     join category in _context.Categories on book.CategoryId equals category.Id
-                     select new BookViewModel()
-                     {
-                         Isbn = book.Isbn,
-                         Title = book.Title,
-                         CategoryName = category.Name,
-                         Price = book.Price
-                     }).ToList();
-
-            dgBooks.ItemsSource = Books;
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
-            int isbn = Convert.ToInt32(((Button)sender).CommandParameter);
+            string isbn = Convert.ToString(((Button)sender).CommandParameter);
+            Book book = _context.Books.Find(isbn);
 
-            MessageBox.Show(isbn.ToString());
+            App.Current.MainWindow.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            App.Current.MainWindow.Opacity = 0.25;
+
+            BookFormModal bookFormModal = new BookFormModal(App.Current.MainWindow, book);
+            bookFormModal.ShowDialog();
+
+            App.Current.MainWindow.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            App.Current.MainWindow.Opacity = 1;
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            int isbn = Convert.ToInt32(((Button)sender).CommandParameter);
+            string isbn = Convert.ToString(((Button)sender).CommandParameter);
 
             App.Current.MainWindow.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
             App.Current.MainWindow.Opacity = 0.25;
 
             DeleteModal deleteModal = new DeleteModal(App.Current.MainWindow, isbn, Helpers.Enums.EntityType.Book);
-            deleteModal.ShowDialog();
+            bool? result = deleteModal.ShowDialog();
 
             App.Current.MainWindow.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
             App.Current.MainWindow.Opacity = 1;
