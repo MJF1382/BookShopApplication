ef867834afaa07d2eadab6916931733618283721
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-16 02:37:56 -0700

    Adding "BookShopDBContext" to data access file.

--- Model/DataAccess.cs
+++ Model/DataAccess.cs
@@ -11,6 +11,8 @@ namespace BookShopApplication.Model
 {
     public static class DataAccess
     {
+        public static BookShopDBContext Context { get; set; }
+
         private static ObservableCollection<BookViewModel> books;
         public static ObservableCollection<BookViewModel> Books
         {
@@ -21,9 +23,47 @@ namespace BookShopApplication.Model
             }
         }
 
+        private static ObservableCollection<BookViewModel> categories;
+        public static ObservableCollection<BookViewModel> Categories
+        {
+            get { return categories; }
+            set
+            {
+                categories = value;
+            }
+        }
+
         static DataAccess()
         {
+            UpdateContext();
             books = new ObservableCollection<BookViewModel>();
+        }
+
+        public static void UpdateContext()
+        {
+            Context = new BookShopDBContext();
+        }
+
+        public static void SetBooks(IEnumerable<BookViewModel> entities)
+        {
+            Books.Clear();
+
+            foreach (BookViewModel entity in entities)
+            {
+                Books.Add(entity);
+            }
+        }
+
+        public static void UpdateBook(BookViewModel entity)
+        {
+            BookViewModel book = Books.Single(P => P.Isbn == entity.Isbn);
+
+            bool result = Books.Remove(book);
+
+            if (result)
+            {
+                Books.Add(entity);
+            }
         }
     }
 }
